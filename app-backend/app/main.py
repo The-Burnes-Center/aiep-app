5003d6b0d8a2d6ec75969ea93bc811b3e267c99f
Kane Xu <xu.hong@northeastern.edu>
2024-09-03 00:19:04 -0400

    Updaded Nginx Configs

--- app-backend/app/main.py
+++ app-backend/app/main.py
@@ -11,7 +11,7 @@ app = FastAPI(root_path='/api')
 # Allow CORS for frontend application
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:3000", "http://a-iep.org"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
