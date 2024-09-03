7ceb215ccae2c8f7d21ad20837ae2330bc35c273
Kane Xu <xu.hong@northeastern.edu>
2024-09-03 00:00:55 -0400

    Updaded Nginx Configs

--- rebuild-server.sh
+++ rebuild-server.sh
@@ -1,4 +1,4 @@
-docker-compose -f docker-compose.local.yml down --remove-orphans
+docker-compose -f docker-compose down --remove-orphans
 sudo git pull
 envsubst '${DOMAIN}' < ./nginx/nginx.conf.template > ./nginx/nginx.conf
-docker-compose -f docker-compose.local.yml up --build -d
+docker-compose -f docker-compose up --build -d
