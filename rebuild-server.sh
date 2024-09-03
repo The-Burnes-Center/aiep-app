7a12d1c44fb0560d8391ce6b048241e1be508fc2
Kane Xu <xu.hong@northeastern.edu>
2024-09-02 23:53:36 -0400

    Finalizing RAG Functionality, To be debugged

--- rebuild-server.sh
+++ rebuild-server.sh
@@ -1,4 +1,4 @@
-sudo docker-compose down
+docker-compose -f docker-compose.local.yml down --remove-orphans
 sudo git pull
 envsubst '${DOMAIN}' < ./nginx/nginx.conf.template > ./nginx/nginx.conf
-sudo docker-compose up --build -d
+docker-compose -f docker-compose.local.yml up --build -d
