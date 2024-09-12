290fe63e94fad722f511d2f09581b993d76e66ac
Kane Xu <xu.hong@northeastern.edu>
2024-09-12 07:15:14 -0400

    testing connection

--- app-backend/app/main.py
+++ app-backend/app/main.py
@@ -11,7 +11,7 @@ app = FastAPI(root_path='/api')
 # Allow CORS for frontend application
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://app-frontend:3000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
