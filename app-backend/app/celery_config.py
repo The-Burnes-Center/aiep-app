5ea95be123baf8af7e5d0224802c66bec77036b3
Kane Xu <xu.hong@northeastern.edu>
2024-09-16 01:42:05 -0400

    Resolved Celery Async Bug Local
    - Moved Celery Job back in Config
    - Configured Redis to run with user account

--- app-backend/app/celery_config.py
+++ app-backend/app/celery_config.py
@@ -1,5 +1,12 @@
 from celery import Celery
-import os
+import os, base64, requests, uuid
+from pdf2image import convert_from_bytes
+from io import BytesIO
+
+from .extract import extract_from_iep
+from fastembed.embedding import DefaultEmbedding
+from qdrant_client import QdrantClient
+from qdrant_client.http.models import Distance, VectorParams, PointStruct
 
 CELERY_BROKER_URL = os.getenv('CELERY_BROKER_URL', 'redis://redis:6379/0')
 CELERY_RESULT_BACKEND = os.getenv('CELERY_RESULT_BACKEND', 'redis://redis:6379/0')
@@ -14,5 +21,49 @@ celery_app.conf.update(
     enable_utc=True,
 )
 
-# Autodiscover tasks in the "tasks" module
-celery_app.autodiscover_tasks(['app'])
+@celery_app.task(name="process_job")
+def process_job(file_data: dict, job_id: str, token: str):
+    base64_images = []
+    for file_bytes in file_data:
+        filename = file_bytes['filename']
+        content = base64.b64decode(file_bytes['content'].encode('utf-8'))
+        
+        if filename.endswith('.pdf'):
+            images = convert_from_bytes(content)
+            for image in images:
+                buffered = BytesIO()
+                image.save(buffered, format="PNG")
+                buffered.seek(0)
+                # Convert bytes to base64
+                base64_image = base64.b64encode(buffered.read()).decode('utf-8')
+                base64_images.append(base64_image)
+        
+        elif filename.endswith(('.jpg', '.jpeg', '.png')):
+            base64_images.append(base64.b64encode(content).decode('utf-8'))
+            
+    results_dict, chunks_dict = extract_from_iep(base64_images)
+
+    # Initialize Qdrant Collection
+    collection_name=f'job_{job_id}'
+    fastembed_model = DefaultEmbedding()
+    fast_embeddings = fastembed_model.embed(chunks_dict.values())
+    qdrant_client = QdrantClient(url='http://qdrant:6333')
+    vector_param = VectorParams(size=384, distance=Distance.DOT)
+    qdrant_client.create_collection(collection_name=collection_name, vectors_config=vector_param)
+    # Prepare points with IDs
+    points = [
+        PointStruct(id=str(uuid.uuid4()), vector=vector, payload={"original_id": chunk_id, "text": chunk})
+        for vector, (chunk_id, chunk) in zip(fast_embeddings, chunks_dict.items())
+    ]
+    # Insert points into the collection using the upsert method
+    qdrant_client.upsert(collection_name=collection_name, points=points)
+
+    # Update the job status with the result
+    response = requests.patch(
+        f"http://app-admin:3000/cms/api/jobs/{job_id}",
+        headers={"Authorization": f"Bearer {token}"},
+        json={"status": "completed", "resultData": {'Result': results_dict}},
+    )
+    
+    if response.status_code != 200:
+        raise Exception("Failed to update job status")
--- app-backend/app/jobs.py
+++ app-backend/app/jobs.py
@@ -1,17 +1,11 @@
-from io import BytesIO
-from pdf2image import convert_from_bytes
-import requests, base64, mimetypes, uuid
-from fastapi import FastAPI, APIRouter, UploadFile, File, Depends, HTTPException, Request, BackgroundTasks
+import requests, base64, mimetypes
+from fastapi import FastAPI, APIRouter, UploadFile, File, Depends, HTTPException, Request
 from pydantic import BaseModel
-from fastembed.embedding import DefaultEmbedding
-from qdrant_client import QdrantClient
-from qdrant_client.http.models import Distance, VectorParams, PointStruct
 
 from typing import List, Dict
 
-from .extract import extract_from_iep
 from .auth import get_current_user
-from .celery_config import celery_app
+from .celery_config import process_job
 
 app = FastAPI()
 router = APIRouter()
@@ -37,7 +31,7 @@ async def create_job(
     for file in files:
         file_content = await file.read()
         mime_type = mimetypes.guess_type(file.filename)[0] or "application/octet-stream"
-        file_data.append({"filename": file.filename, "content": file_content, "mime_type": mime_type})
+        file_data.append({"filename": file.filename, "content": base64.b64encode(file_content).decode('utf-8'), "mime_type": mime_type})
 
         response = requests.post(
             "http://app-admin:3000/cms/api/media",
@@ -72,53 +66,6 @@ async def create_job(
 
     # Immediately return a response to the client
     return {"job_id": job_id, "status": "Job started, processing in background"}
-
-@celery_app.task
-def process_job(file_data: bytes, job_id: str, token: str):
-    base64_images = []
-    for file_bytes in file_data:
-        filename = file_bytes['filename']
-        content = file_bytes['content']
-        
-        if filename.endswith('.pdf'):
-            images = convert_from_bytes(content)
-            for image in images:
-                buffered = BytesIO()
-                image.save(buffered, format="PNG")
-                buffered.seek(0)
-                # Convert bytes to base64
-                base64_image = base64.b64encode(buffered.read()).decode('utf-8')
-                base64_images.append(base64_image)
-        
-        elif filename.endswith(('.jpg', '.jpeg', '.png')):
-            base64_images.append(base64.b64encode(content).decode('utf-8'))
-            
-    results_dict, chunks_dict = extract_from_iep(base64_images)
-
-    # Initialize Qdrant Collection
-    collection_name=f'job_{job_id}'
-    fastembed_model = DefaultEmbedding()
-    fast_embeddings = fastembed_model.embed(chunks_dict.values())
-    qdrant_client = QdrantClient(url='http://qdrant:6333')
-    vector_param = VectorParams(size=384, distance=Distance.DOT)
-    qdrant_client.create_collection(collection_name=collection_name, vectors_config=vector_param)
-    # Prepare points with IDs
-    points = [
-        PointStruct(id=str(uuid.uuid4()), vector=vector, payload={"original_id": chunk_id, "text": chunk})
-        for vector, (chunk_id, chunk) in zip(fast_embeddings, chunks_dict.items())
-    ]
-    # Insert points into the collection using the upsert method
-    qdrant_client.upsert(collection_name=collection_name, points=points)
-
-    # Update the job status with the result
-    response = requests.patch(
-        f"http://app-admin:3000/cms/api/jobs/{job_id}",
-        headers={"Authorization": f"Bearer {token}"},
-        json={"status": "completed", "resultData": {'Result': results_dict}},
-    )
-    
-    if response.status_code != 200:
-        raise Exception("Failed to update job status")
 
 @router.get("/get-all", dependencies=[Depends(get_current_user)])
 async def get_user_jobs(
